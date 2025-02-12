47cdfe4ee444bfa61f4ad4a8f5beb15a49c2287a
Jakub Kadela <kadelakuba@gmail.com>
2025-02-12 16:40:00 +0100

    socials - add target blank

--- app/src/components/socials/socials.tsx
+++ app/src/components/socials/socials.tsx
@@ -5,12 +5,12 @@ import {SocialsBoxStyle} from "@kadela/components/socials/socialsBoxStyle";
 export function Socials() {
     return (
         <Box display="flex" gap={2} justifyContent={"right"}>
-            <a href={'https://www.linkedin.com/in/jakub-kadela/'}>
+            <a href={'https://www.linkedin.com/in/jakub-kadela/'} target="_blank">
                 <SocialsBoxStyle>
                     <FaLinkedin size={40} />
                 </SocialsBoxStyle>
             </a>
-            <a href={'https://github.com/KadelaKuba'}>
+            <a href={'https://github.com/KadelaKuba'} target="_blank">
                 <SocialsBoxStyle>
                     <FaGithub size={40}/>
                 </SocialsBoxStyle>
